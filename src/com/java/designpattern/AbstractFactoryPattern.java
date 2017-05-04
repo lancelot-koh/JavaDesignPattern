65d7170bd3685512e73594ce00ccb8902c2f38f5
Yong <lcfgaoyong@gmail.com>
2017-05-04 13:41:33 +0800

    init

---
+++ src/com/java/designpattern/AbstractFactoryPattern.java
@@ -0,0 +1,15 @@
+package com.java.designpattern;
+
+/**
+ * Created by i077407 on 4/5/17.
+ */
+public class AbstractFactoryPattern {
+}
+
+interface AbstractFactory {
+
+}
+
+interface AbstractProduct{
+
+}
